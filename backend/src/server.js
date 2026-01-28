e662f37b6206489007ae1a4cf649a2126569bfd9
Nilesh <nileshgoyal624@gmail.com>
2026-01-28 19:20:46 +0530

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+const app = express();
+
+app.get("/health",(req,res)=>{
+    res.status(200).json({msg:"api is running "})
+})
+
+app.listen(ENV.PORT,()=> console.log("Server is running on port",ENV.PORT));
