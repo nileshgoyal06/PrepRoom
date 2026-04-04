1283fc7e3904495920b76d415d01e3b8fca87a8e
Nilesh <nileshgoyal624@gmail.com>
2026-04-04 16:06:23 +0530

    DB and tool setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,20 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 const app = express();
 
 app.get("/health",(req,res)=>{
     res.status(200).json({msg:"api is running "})
 })
 
-app.listen(ENV.PORT,()=> console.log("Server is running on port",ENV.PORT));
+const startServer = async()=>{
+    try{
+        await connectDB();
+        app.listen(ENV.PORT, ()=>console.log("Server is running on port:",ENV.PORT));
+    } 
+    catch(error){
+        console.error(error);
+    }
+};
+
+startServer();
