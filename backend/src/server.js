639f5d86e84ef50eb62ac9a26a22fe70d7c9e186
Nilesh <nileshgoyal624@gmail.com>
2026-04-05 22:06:04 +0530

    Oauth setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,16 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { inngest,functions } from "./lib/inngest.js";
+import { serve } from "inngest/express";
+//import { functions } from "./inngest/functions.js";
 const app = express();
 
+app.use(express.json())
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+app.use("/api/inngest",serve({client:inngest,functions}));
 app.get("/health",(req,res)=>{
     res.status(200).json({msg:"api is running "})
 })
