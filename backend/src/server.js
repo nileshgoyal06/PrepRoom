d93e550b33c657dbddb6c13c8e375224ac7bf10d
Nilesh <nileshgoyal624@gmail.com>
2026-04-05 22:22:19 +0530

    server.js update

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 
 app.use("/api/inngest",serve({client:inngest,functions}));
-app.get("/health",(req,res)=>{
+app.get("/",(req,res)=>{
     res.status(200).json({msg:"api is running "})
 })
 
